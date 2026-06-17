cf7ce7c34882f9d0281762cad9b51740eaa33e4b
gabrieloliveira <go742743@gmail.com>
2026-06-16 22:03:27 -0300

    estudando

---
+++ manipulacoes, arrow/metodos/matematica/aula1.js
@@ -0,0 +1,12 @@
+let novoValor = Math.floor(Math.random() * 100)
+console.log(novoValor)
+
+// Math.round() -> arredonda para o inteiro mais proximo
+// Math.floor() -> arredonda para baixo
+// Math.ceil() -> arredonda para cima
+// Math.trunc() -> remove a parte decimal0000000000000000000000
+// Math.min(1,2,3) -> retorna o menor numero entre os parametros
+// Math.max(1,2,3) -> retorna o maior numero entre os parametros
+// Math.random() -> retorna um numero aleatorio entre 0 e 1
+// Math.pow(2,2) -> retorna a potencia de um numero
+// Math.sqrt(4) -> retorna a raiz quadrada de um numero
